ae3b1418a80bec8b8168edb3d5243e76d1d863c8
Will Leinweber <will@bitfission.com>
2014-01-23 23:07:18 -0800

    format

--- newlevel0.c
+++ newlevel0.c
@@ -26,13 +26,13 @@ size_t useWalloc = 1;
 
 int  currentOffset = 0;
 void *walloc(int numBytes) {
-    if (useWalloc) {
-      char *ptr = (void *) ADDR + currentOffset;
-      currentOffset += numBytes + (16 - (numBytes%16));
-      return ptr;
-    } else {
-      return malloc(numBytes);
-    }
+  if (useWalloc) {
+    char *ptr = (void *) ADDR + currentOffset;
+    currentOffset += numBytes + (16 - (numBytes%16));
+    return ptr;
+  } else {
+    return malloc(numBytes);
+  }
 }
 
 #ifndef CRITBIT_H_
@@ -42,16 +42,16 @@ void *walloc(int numBytes) {
 extern "C" {
 #endif
 
-//typedef struct {
-//  void *root;
-//} critbit0_tree;
+  //typedef struct {
+  //  void *root;
+  //} critbit0_tree;
 
-//int critbit0_contains(critbit0_tree *t, const char *u);
-//int critbit0_insert(critbit0_tree *t, const char *u);
-//int critbit0_delete(critbit0_tree *t, const char *u);
-//void critbit0_clear(critbit0_tree *t);
-//int critbit0_allprefixed(critbit0_tree *t, const char *prefix,
-//                         int (*handle) (const char *, void *), void *arg);
+  //int critbit0_contains(critbit0_tree *t, const char *u);
+  //int critbit0_insert(critbit0_tree *t, const char *u);
+  //int critbit0_delete(critbit0_tree *t, const char *u);
+  //void critbit0_clear(critbit0_tree *t);
+  //int critbit0_allprefixed(critbit0_tree *t, const char *prefix,
+  //                         int (*handle) (const char *, void *), void *arg);
 
 #ifdef _cplusplus
 };
@@ -65,51 +65,51 @@ extern "C" {
 
 
 typedef struct{
-void*child[2];
-uint32 byte;
-uint8 otherbits;
+  void*child[2];
+  uint32 byte;
+  uint8 otherbits;
 }critbit0_node;
 
 typedef struct{
-void*root;
+  void*root;
 }critbit0_tree;
 
 /*:2*//*3:*/
 
 int
 critbit0_contains(critbit0_tree*t,const char*u){
-const uint8*ubytes= (void*)u;
-const size_t ulen= strlen(u);
-uint8*p= t->root;
+  const uint8*ubytes= (void*)u;
+  const size_t ulen= strlen(u);
+  uint8*p= t->root;
 
-/*4:*/
+  /*4:*/
 
-if(!p)return 0;
+  if(!p)return 0;
 
-/*:4*/
+  /*:4*/
 
-/*5:*/
+  /*5:*/
 
-while(1&(intptr_t)p){
-critbit0_node*q= (void*)(p-1);
-/*6:*/
+  while(1&(intptr_t)p){
+    critbit0_node*q= (void*)(p-1);
+    /*6:*/
 
-uint8 c= 0;
-if(q->byte<ulen)c= ubytes[q->byte];
-const int direction= (1+(q->otherbits|c))>>8;
+    uint8 c= 0;
+    if(q->byte<ulen)c= ubytes[q->byte];
+    const int direction= (1+(q->otherbits|c))>>8;
 
-/*:6*/
+    /*:6*/
 
-p= q->child[direction];
-}
+    p= q->child[direction];
+  }
 
-/*:5*/
+  /*:5*/
 
-/*7:*/
+  /*7:*/
 
-return 0==strcmp(u,(const char*)p);
+  return 0==strcmp(u,(const char*)p);
 
-/*:7*/
+  /*:7*/
 
 }
 
@@ -117,264 +117,264 @@ return 0==strcmp(u,(const char*)p);
 
 int critbit0_insert(critbit0_tree*t,const char*u)
 {
-const uint8*const ubytes= (void*)u;
-const size_t ulen= strlen(u);
-uint8*p= t->root;
+  const uint8*const ubytes= (void*)u;
+  const size_t ulen= strlen(u);
+  uint8*p= t->root;
 
-/*9:*/
+  /*9:*/
 
-if(!p){
-char*x = walloc(ulen+1);
-//int a= posix_memalign((void**)&x,sizeof(void*),ulen+1);
-memcpy(x,u,ulen+1);
-t->root= x;
-return 2;
-}
+  if(!p){
+    char*x = walloc(ulen+1);
+    //int a= posix_memalign((void**)&x,sizeof(void*),ulen+1);
+    memcpy(x,u,ulen+1);
+    t->root= x;
+    return 2;
+  }
 
-/*:9*/
+  /*:9*/
 
-/*5:*/
+  /*5:*/
 
-while(1&(intptr_t)p){
-critbit0_node*q= (void*)(p-1);
-/*6:*/
+  while(1&(intptr_t)p){
+    critbit0_node*q= (void*)(p-1);
+    /*6:*/
 
-uint8 c= 0;
-if(q->byte<ulen)c= ubytes[q->byte];
-const int direction= (1+(q->otherbits|c))>>8;
+    uint8 c= 0;
+    if(q->byte<ulen)c= ubytes[q->byte];
+    const int direction= (1+(q->otherbits|c))>>8;
 
-/*:6*/
+    /*:6*/
 
-p= q->child[direction];
-}
+    p= q->child[direction];
+  }
 
-/*:5*/
+  /*:5*/
 
-/*10:*/
+  /*10:*/
 
-/*11:*/
+  /*11:*/
 
-uint32 newbyte;
-uint32 newotherbits;
+  uint32 newbyte;
+  uint32 newotherbits;
 
-for(newbyte= 0;newbyte<ulen;++newbyte){
-if(p[newbyte]!=ubytes[newbyte]){
-newotherbits= p[newbyte]^ubytes[newbyte];
-goto different_byte_found;
-}
-}
+  for(newbyte= 0;newbyte<ulen;++newbyte){
+    if(p[newbyte]!=ubytes[newbyte]){
+      newotherbits= p[newbyte]^ubytes[newbyte];
+      goto different_byte_found;
+    }
+  }
 
-if(p[newbyte]!=0){
-newotherbits= p[newbyte];
-goto different_byte_found;
-}
-return 1;
+  if(p[newbyte]!=0){
+    newotherbits= p[newbyte];
+    goto different_byte_found;
+  }
+  return 1;
 
 different_byte_found:
 
-/*:11*/
+  /*:11*/
 
-/*12:*/
+  /*12:*/
 
-newotherbits|= newotherbits>>1;
-newotherbits|= newotherbits>>2;
-newotherbits|= newotherbits>>4;
-newotherbits= (newotherbits&~(newotherbits>>1))^255;
-uint8 c= p[newbyte];
-int newdirection= (1+(newotherbits|c))>>8;
+  newotherbits|= newotherbits>>1;
+  newotherbits|= newotherbits>>2;
+  newotherbits|= newotherbits>>4;
+  newotherbits= (newotherbits&~(newotherbits>>1))^255;
+  uint8 c= p[newbyte];
+  int newdirection= (1+(newotherbits|c))>>8;
 
-/*:12*/
-
-
-/*:10*/
-
-/*13:*/
-
-/*14:*/
-
-//critbit0_node*newnode;
-//if(posix_memalign((void**)&newnode,sizeof(void*),sizeof(critbit0_node)))return 0;
-critbit0_node*newnode = walloc(sizeof(critbit0_node));
-
-char*x = walloc(ulen+1);
-//if(posix_memalign((void**)&x,sizeof(void*),ulen+1)){
-//free(newnode);
-//return 0;
-//}
-memcpy(x,ubytes,ulen+1);
-
-newnode->byte= newbyte;
-newnode->otherbits= newotherbits;
-newnode->child[1-newdirection]= x;
-
-/*:14*/
-
-/*15:*/
-
-void**wherep= &t->root;
-for(;;){
-uint8*p= *wherep;
-if(!(1&(intptr_t)p))break;
-critbit0_node*q= (void*)(p-1);
-if(q->byte> newbyte)break;
-if(q->byte==newbyte&&q->otherbits> newotherbits)break;
-uint8 c= 0;
-if(q->byte<ulen)c= ubytes[q->byte];
-const int direction= (1+(q->otherbits|c))>>8;
-wherep= q->child+direction;
-}
-
-newnode->child[newdirection]= *wherep;
-*wherep= (void*)(1+(char*)newnode);
-
-/*:15*/
+  /*:12*/
 
 
-/*:13*/
+  /*:10*/
+
+  /*13:*/
+
+  /*14:*/
+
+  //critbit0_node*newnode;
+  //if(posix_memalign((void**)&newnode,sizeof(void*),sizeof(critbit0_node)))return 0;
+  critbit0_node*newnode = walloc(sizeof(critbit0_node));
+
+  char*x = walloc(ulen+1);
+  //if(posix_memalign((void**)&x,sizeof(void*),ulen+1)){
+  //free(newnode);
+  //return 0;
+  //}
+  memcpy(x,ubytes,ulen+1);
+
+  newnode->byte= newbyte;
+  newnode->otherbits= newotherbits;
+  newnode->child[1-newdirection]= x;
+
+  /*:14*/
+
+  /*15:*/
+
+  void**wherep= &t->root;
+  for(;;){
+    uint8*p= *wherep;
+    if(!(1&(intptr_t)p))break;
+    critbit0_node*q= (void*)(p-1);
+    if(q->byte> newbyte)break;
+    if(q->byte==newbyte&&q->otherbits> newotherbits)break;
+    uint8 c= 0;
+    if(q->byte<ulen)c= ubytes[q->byte];
+    const int direction= (1+(q->otherbits|c))>>8;
+    wherep= q->child+direction;
+  }
+
+  newnode->child[newdirection]= *wherep;
+  *wherep= (void*)(1+(char*)newnode);
+
+  /*:15*/
 
 
-return 2;
+  /*:13*/
+
+
+  return 2;
 }
 
 /*:8*//*16:*/
 
 int critbit0_delete(critbit0_tree*t,const char*u){
-const uint8*ubytes= (void*)u;
-const size_t ulen= strlen(u);
-uint8*p= t->root;
-void**wherep= &t->root;
-void**whereq= 0;
-critbit0_node*q= 0;
-int direction= 0;
+  const uint8*ubytes= (void*)u;
+  const size_t ulen= strlen(u);
+  uint8*p= t->root;
+  void**wherep= &t->root;
+  void**whereq= 0;
+  critbit0_node*q= 0;
+  int direction= 0;
 
-/*17:*/
+  /*17:*/
 
-if(!p)return 0;
+  if(!p)return 0;
 
-/*:17*/
+  /*:17*/
 
-/*18:*/
+  /*18:*/
 
-while(1&(intptr_t)p){
-whereq= wherep;
-q= (void*)(p-1);
-uint8 c= 0;
-if(q->byte<ulen)c= ubytes[q->byte];
-direction= (1+(q->otherbits|c))>>8;
-wherep= q->child+direction;
-p= *wherep;
-}
+  while(1&(intptr_t)p){
+    whereq= wherep;
+    q= (void*)(p-1);
+    uint8 c= 0;
+    if(q->byte<ulen)c= ubytes[q->byte];
+    direction= (1+(q->otherbits|c))>>8;
+    wherep= q->child+direction;
+    p= *wherep;
+  }
 
-/*:18*/
+  /*:18*/
 
-/*19:*/
+  /*19:*/
 
-if(0!=strcmp(u,(const char*)p))return 0;
-free(p);
+  if(0!=strcmp(u,(const char*)p))return 0;
+  free(p);
 
-/*:19*/
+  /*:19*/
 
-/*20:*/
+  /*20:*/
 
-if(!whereq){
-t->root= 0;
-return 1;
-}
+  if(!whereq){
+    t->root= 0;
+    return 1;
+  }
 
-*whereq= q->child[1-direction];
-free(q);
+  *whereq= q->child[1-direction];
+  free(q);
 
-/*:20*/
+  /*:20*/
 
 
-return 1;
+  return 1;
 }
 
 /*:16*//*21:*/
 
 static void
 traverse(void*top){
-/*22:*/
+  /*22:*/
 
-uint8*p= top;
+  uint8*p= top;
 
-if(1&(intptr_t)p){
-critbit0_node*q= (void*)(p-1);
-traverse(q->child[0]);
-traverse(q->child[1]);
-free(q);
-}else{
-free(p);
-}
+  if(1&(intptr_t)p){
+    critbit0_node*q= (void*)(p-1);
+    traverse(q->child[0]);
+    traverse(q->child[1]);
+    free(q);
+  }else{
+    free(p);
+  }
 
-/*:22*/
+  /*:22*/
 
 }
 
 void critbit0_clear(critbit0_tree*t)
 {
-if(t->root)traverse(t->root);
-t->root= NULL;
+  if(t->root)traverse(t->root);
+  t->root= NULL;
 }
 
 /*:21*//*23:*/
 
 static int
 allprefixed_traverse(uint8*top,
-int(*handle)(const char*,void*),void*arg){
-/*26:*/
+    int(*handle)(const char*,void*),void*arg){
+  /*26:*/
 
-if(1&(intptr_t)top){
-critbit0_node*q= (void*)(top-1);
-int direction;
-for(direction= 0;direction<2;++direction)
-switch(allprefixed_traverse(q->child[direction],handle,arg)){
-case 1:break;
-case 0:return 0;
-default:return-1;
-}
-return 1;
-}
+  if(1&(intptr_t)top){
+    critbit0_node*q= (void*)(top-1);
+    int direction;
+    for(direction= 0;direction<2;++direction)
+      switch(allprefixed_traverse(q->child[direction],handle,arg)){
+        case 1:break;
+        case 0:return 0;
+        default:return-1;
+      }
+    return 1;
+  }
 
-/*27:*/
+  /*27:*/
 
-return handle((const char*)top,arg);/*:27*/
+  return handle((const char*)top,arg);/*:27*/
 
 }
 
 int
 critbit0_allprefixed(critbit0_tree*t,const char*prefix,
-int(*handle)(const char*,void*),void*arg){
-const uint8*ubytes= (void*)prefix;
-const size_t ulen= strlen(prefix);
-uint8*p= t->root;
-uint8*top= p;
+    int(*handle)(const char*,void*),void*arg){
+  const uint8*ubytes= (void*)prefix;
+  const size_t ulen= strlen(prefix);
+  uint8*p= t->root;
+  uint8*top= p;
 
-if(!p)return 1;
-/*24:*/
+  if(!p)return 1;
+  /*24:*/
 
-while(1&(intptr_t)p){
-critbit0_node*q= (void*)(p-1);
-uint8 c= 0;
-if(q->byte<ulen)c= ubytes[q->byte];
-const int direction= (1+(q->otherbits|c))>>8;
-p= q->child[direction];
-if(q->byte<ulen)top= p;
-}
+  while(1&(intptr_t)p){
+    critbit0_node*q= (void*)(p-1);
+    uint8 c= 0;
+    if(q->byte<ulen)c= ubytes[q->byte];
+    const int direction= (1+(q->otherbits|c))>>8;
+    p= q->child[direction];
+    if(q->byte<ulen)top= p;
+  }
 
-/*:24*/
+  /*:24*/
 
-/*25:*/
+  /*25:*/
 
-size_t i;
-for(i= 0;i<ulen;++i){
-if(p[i]!=ubytes[i])return 1;
-}
+  size_t i;
+  for(i= 0;i<ulen;++i){
+    if(p[i]!=ubytes[i])return 1;
+  }
 
-/*:25*/
+  /*:25*/
 
 
-return allprefixed_traverse(top,handle,arg);
+  return allprefixed_traverse(top,handle,arg);
 }
 
 /*:23*/
@@ -385,45 +385,45 @@ return allprefixed_traverse(top,handle,arg);
 
 
 int main(int argc,char *argv[]) {
-int fd;
-fd = open("mem",  O_RDWR);
-if (argc==3) {
-void * s = mmap((void *) ADDR, MAX_DATA, PROT_READ | PROT_WRITE,  MAP_SHARED | MAP_FIXED, fd, 0);
-} else {
-void * s = mmap((void *) ADDR, MAX_DATA, PROT_READ | PROT_WRITE,  MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
-//printf("mmap ret: %zi, %s\n", s, strerror(errno));
-int len = read(fd, (void *) ADDR, MAX_DATA);
-//printf("read ret: %d, %s\n", len, strerror(errno));
-}
-
-   FILE *fp;
-   char word[80];
-   char lword[80];
-   int c;
-   int i;
-
-critbit0_tree tree = {0};
- // printf("\npointer %zu", tree.root);
-
-void **treeRoot = walloc(sizeof(void *));
-//printf("\nw pointer %zu", treeRoot);
-
-if (argc==3) {
- // printf("\npointer %zu", tree.root);
-  FILE *dict;
-  dict = fopen(argv[1],"r");
-  while (fscanf(dict, "%s", word) == 1) {
-    if (!isupper(word[0])) { critbit0_insert(&tree, word); }
+  int fd;
+  fd = open("mem",  O_RDWR);
+  if (argc==3) {
+    void * s = mmap((void *) ADDR, MAX_DATA, PROT_READ | PROT_WRITE,  MAP_SHARED | MAP_FIXED, fd, 0);
+  } else {
+    void * s = mmap((void *) ADDR, MAX_DATA, PROT_READ | PROT_WRITE,  MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
+    //printf("mmap ret: %zi, %s\n", s, strerror(errno));
+    int len = read(fd, (void *) ADDR, MAX_DATA);
+    //printf("read ret: %d, %s\n", len, strerror(errno));
   }
-// printf("\npointer %zu", tree.root);
- *treeRoot = tree.root;
- printf("%d", currentOffset);
 
- return 0;
-} else {
-  tree.root = *treeRoot;
-// printf("\npointer %zu", tree.root);
-}
+  FILE *fp;
+  char word[80];
+  char lword[80];
+  int c;
+  int i;
+
+  critbit0_tree tree = {0};
+  // printf("\npointer %zu", tree.root);
+
+  void **treeRoot = walloc(sizeof(void *));
+  //printf("\nw pointer %zu", treeRoot);
+
+  if (argc==3) {
+    // printf("\npointer %zu", tree.root);
+    FILE *dict;
+    dict = fopen(argv[1],"r");
+    while (fscanf(dict, "%s", word) == 1) {
+      if (!isupper(word[0])) { critbit0_insert(&tree, word); }
+    }
+    // printf("\npointer %zu", tree.root);
+    *treeRoot = tree.root;
+    printf("%d", currentOffset);
+
+    return 0;
+  } else {
+    tree.root = *treeRoot;
+    // printf("\npointer %zu", tree.root);
+  }
 
 
   i = 0;
@@ -433,7 +433,7 @@ if (argc==3) {
   size_t lenread = 0;
   size_t readthistime;
   while ((readthistime = read(0, input + lenread, 163840)))
-       lenread += readthistime;
+    lenread += readthistime;
 
   while(1) {
     if (j == lenread) { break; }
@@ -461,5 +461,5 @@ if (argc==3) {
     j++;
   }
 
-   return(0);
+  return(0);
 }
