27d058aaa388f36427093db1ab72dd67f45a4834
Will Leinweber <will@bitfission.com>
2014-01-23 18:03:41 -0800

    try static

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 # Add any build steps you need here
 dd if=/dev/zero of=mem bs=10485760 count=1
-cc -O2 newlevel0.c -o level0
+cc -static-libgcc -O2 newlevel0.c -o level0
+
 ./level0 /usr/share/dict/words build
