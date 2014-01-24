4bbdf0dda309221d348db3c53fa9c8905eda6ac9
Will Leinweber <will@bitfission.com>
2014-01-24 12:11:54 -0800

    build with -lm on linux

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ Darwin = `uname -s` ]; then
 cc -O0 -g newlevel0.c -o level0 -D_DARWIN_C_SOURCE
 else
 #cc -static -static-libgcc -O3 newlevel0.c -o level0 -DGNU_SOURCE
-cc -O3 newlevel0.c -o level0 -DGNU_SOURCE
+cc -O3 newlevel0.c -o level0 -DGNU_SOURCE -lm
 fi
 
 ./level0 /usr/share/dict/words build
