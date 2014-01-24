6bb80da56817ab532180beb1dc01a880cd7d0295
Will Leinweber <will@bitfission.com>
2014-01-24 14:56:44 -0800

    not static

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ dd if=/dev/zero of=mem bs=504928 count=1
 if [ Darwin = `uname -s` ]; then
 cc -O0 -g newlevel0.c -o level0 -D_DARWIN_C_SOURCE
 else
-#cc -O3 newlevel0.c -o level0 -DGNU_SOURCE -lm
-cc -O3 -static -static-libgcc -s newlevel0.c -o level0 -DGNU_SOURCE -lm
+cc -O3 newlevel0.c -o level0 -DGNU_SOURCE -lm
+#cc -O3 -static -static-libgcc -s newlevel0.c -o level0 -DGNU_SOURCE -lm
 fi
 
 ./level0 /usr/share/dict/words build
