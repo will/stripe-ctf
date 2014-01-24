80c507dfe59550d9e8edc6e693a64e95e7e92568
Will Leinweber <will@bitfission.com>
2014-01-24 10:55:42 -0800

    bloom start, no walloc

--- newlevel0.c
+++ newlevel0.c
@@ -18,6 +18,7 @@
 #include <sys/mman.h> /* mmap() is defined in this header */
 #include <fcntl.h>
 
+
 // 10 MB of data that can be allocated
 //define MAX_DATA 10485760 //10MB
 #define MAX_DATA (10*1048576) // x1MB
@@ -36,373 +37,234 @@ void *walloc(int numBytes) {
   }
 }
 
-#ifndef CRITBIT_H_
-#define CRITBIT_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
-
-  //typedef struct {
-  //  void *root;
-  //} critbit0_tree;
-
-  //int critbit0_contains(critbit0_tree *t, const char *u);
-  //int critbit0_insert(critbit0_tree *t, const char *u);
-  //int critbit0_delete(critbit0_tree *t, const char *u);
-  //void critbit0_clear(critbit0_tree *t);
-  //int critbit0_allprefixed(critbit0_tree *t, const char *prefix,
-  //                         int (*handle) (const char *, void *), void *arg);
-
-#ifdef _cplusplus
-};
-#endif
-
-#endif  // CRITBIT_H_
-#define _POSIX_C_SOURCE 200112
-#define uint8 uint8_t
-#define uint32 uint32_t
-/*2:*/
 
 
-typedef struct{
-  void*child[2];
-  uint32 byte;
-  uint8 otherbits;
-}critbit0_node;
 
-typedef struct{
-  void*root;
-}critbit0_tree;
 
-/*:2*//*3:*/
 
-int
-critbit0_contains(critbit0_tree*t,const char*u){
-  const uint8*ubytes= (void*)u;
-  const size_t ulen= strlen(u);
-  uint8*p= t->root;
 
-  /*4:*/
+//-----------------------------------------------------------------------------
+// MurmurHash2, by Austin Appleby
 
-  if(!p)return 0;
+// Note - This code makes a few assumptions about how your machine behaves -
 
-  /*:4*/
+// 1. We can read a 4-byte value from any address without crashing
+// 2. sizeof(int) == 4
 
-  /*5:*/
+// And it has a few limitations -
 
-  while(1&(intptr_t)p){
-    critbit0_node*q= (void*)(p-1);
-    /*6:*/
+// 1. It will not work incrementally.
+// 2. It will not produce the same results on little-endian and big-endian
+//    machines.
 
-    uint8 c= 0;
-    if(q->byte<ulen)c= ubytes[q->byte];
-    const int direction= (1+(q->otherbits|c))>>8;
-
-    /*:6*/
-
-    p= q->child[direction];
-  }
-
-  /*:5*/
-
-  /*7:*/
-
-  return 0==strcmp(u,(const char*)p);
-
-  /*:7*/
-
-}
-
-/*:3*//*8:*/
-
-int critbit0_insert(critbit0_tree*t,const char*u)
+unsigned int murmurhash2(const void * key, int len, const unsigned int seed)
 {
-  const uint8*const ubytes= (void*)u;
-  const size_t ulen= strlen(u);
-  uint8*p= t->root;
+	// 'm' and 'r' are mixing constants generated offline.
+	// They're not really 'magic', they just happen to work well.
 
-  /*9:*/
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
 
-  if(!p){
-    char*x = walloc(ulen+1);
-    //int a= posix_memalign((void**)&x,sizeof(void*),ulen+1);
-    memcpy(x,u,ulen+1);
-    t->root= x;
-    return 2;
+	// Initialize the hash to a 'random' value
+
+	unsigned int h = seed ^ len;
+
+	// Mix 4 bytes at a time into the hash
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	while(len >= 4)
+	{
+		unsigned int k = *(unsigned int *)data;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+
+	// Handle the last few bytes of the input array
+
+	switch(len)
+	{
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0];
+	        h *= m;
+	};
+
+	// Do a few final mixes of the hash to ensure the last few
+	// bytes are well-incorporated.
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
+}
+/*
+ *  Copyright (c) 2012, Jyri J. Virkki
+ *  All rights reserved.
+ *
+ *  This file is under BSD license. See LICENSE file.
+ */
+
+/*
+ * Refer to bloom.h for documentation on the public interfaces.
+ */
+
+#include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct bloom
+{
+  // These fields are part of the public interface of this structure.
+  // Client code may read these values if desired. Client code MUST NOT
+  // modify any of these.
+  int entries;
+  double error;
+  int bits;
+  int bytes;
+  int hashes;
+
+  // Fields below are private to the implementation. These may go away or
+  // change incompatibly at any moment. Client code MUST NOT access or rely
+  // on these.
+  double bpe;
+  unsigned char * bf;
+  int ready;
+};
+
+
+
+static int bloom_check_add(struct bloom * bloom,
+                           const void * buffer, int len, int add)
+{
+  if (bloom->ready == 0) {
+    (void)printf("bloom at %p not initialized!\n", (void *)bloom);
+    return -1;
   }
 
-  /*:9*/
+  int hits = 0;
+  register unsigned int a = murmurhash2(buffer, len, 0x9747b28c);
+  register unsigned int b = murmurhash2(buffer, len, a);
+  register unsigned int x;
+  register unsigned int i;
+  register unsigned int byte;
+  register unsigned int mask;
+  register unsigned char c;
 
-  /*5:*/
+  for (i = 0; i < bloom->hashes; i++) {
+    x = (a + i*b) % bloom->bits;
+    byte = x >> 3;
+    c = bloom->bf[byte];        // expensive memory access
+    mask = 1 << (x % 8);
 
-  while(1&(intptr_t)p){
-    critbit0_node*q= (void*)(p-1);
-    /*6:*/
-
-    uint8 c= 0;
-    if(q->byte<ulen)c= ubytes[q->byte];
-    const int direction= (1+(q->otherbits|c))>>8;
-
-    /*:6*/
-
-    p= q->child[direction];
-  }
-
-  /*:5*/
-
-  /*10:*/
-
-  /*11:*/
-
-  uint32 newbyte;
-  uint32 newotherbits;
-
-  for(newbyte= 0;newbyte<ulen;++newbyte){
-    if(p[newbyte]!=ubytes[newbyte]){
-      newotherbits= p[newbyte]^ubytes[newbyte];
-      goto different_byte_found;
+    if (c & mask) {
+      hits++;
+    } else {
+      if (add) {
+        bloom->bf[byte] = c | mask;
+      }
     }
   }
 
-  if(p[newbyte]!=0){
-    newotherbits= p[newbyte];
-    goto different_byte_found;
-  }
-  return 1;
-
-different_byte_found:
-
-  /*:11*/
-
-  /*12:*/
-
-  newotherbits|= newotherbits>>1;
-  newotherbits|= newotherbits>>2;
-  newotherbits|= newotherbits>>4;
-  newotherbits= (newotherbits&~(newotherbits>>1))^255;
-  uint8 c= p[newbyte];
-  int newdirection= (1+(newotherbits|c))>>8;
-
-  /*:12*/
-
-
-  /*:10*/
-
-  /*13:*/
-
-  /*14:*/
-
-  //critbit0_node*newnode;
-  //if(posix_memalign((void**)&newnode,sizeof(void*),sizeof(critbit0_node)))return 0;
-  critbit0_node*newnode = walloc(sizeof(critbit0_node));
-
-  char*x = walloc(ulen+1);
-  //if(posix_memalign((void**)&x,sizeof(void*),ulen+1)){
-  //free(newnode);
-  //return 0;
-  //}
-  memcpy(x,ubytes,ulen+1);
-
-  newnode->byte= newbyte;
-  newnode->otherbits= newotherbits;
-  newnode->child[1-newdirection]= x;
-
-  /*:14*/
-
-  /*15:*/
-
-  void**wherep= &t->root;
-  for(;;){
-    uint8*p= *wherep;
-    if(!(1&(intptr_t)p))break;
-    critbit0_node*q= (void*)(p-1);
-    if(q->byte> newbyte)break;
-    if(q->byte==newbyte&&q->otherbits> newotherbits)break;
-    uint8 c= 0;
-    if(q->byte<ulen)c= ubytes[q->byte];
-    const int direction= (1+(q->otherbits|c))>>8;
-    wherep= q->child+direction;
+  if (hits == bloom->hashes) {
+    return 1;                   // 1 == element already in (or collision)
   }
 
-  newnode->child[newdirection]= *wherep;
-  *wherep= (void*)(1+(char*)newnode);
-
-  /*:15*/
-
-
-  /*:13*/
-
-
-  return 2;
+  return 0;
 }
 
-/*:8*//*16:*/
 
-int critbit0_delete(critbit0_tree*t,const char*u){
-  const uint8*ubytes= (void*)u;
-  const size_t ulen= strlen(u);
-  uint8*p= t->root;
-  void**wherep= &t->root;
-  void**whereq= 0;
-  critbit0_node*q= 0;
-  int direction= 0;
-
-  /*17:*/
-
-  if(!p)return 0;
-
-  /*:17*/
-
-  /*18:*/
-
-  while(1&(intptr_t)p){
-    whereq= wherep;
-    q= (void*)(p-1);
-    uint8 c= 0;
-    if(q->byte<ulen)c= ubytes[q->byte];
-    direction= (1+(q->otherbits|c))>>8;
-    wherep= q->child+direction;
-    p= *wherep;
-  }
-
-  /*:18*/
-
-  /*19:*/
-
-  if(0!=strcmp(u,(const char*)p))return 0;
-  free(p);
-
-  /*:19*/
-
-  /*20:*/
-
-  if(!whereq){
-    t->root= 0;
-    return 1;
-  }
-
-  *whereq= q->child[1-direction];
-  free(q);
-
-  /*:20*/
-
-
-  return 1;
-}
-
-/*:16*//*21:*/
-
-static void
-traverse(void*top){
-  /*22:*/
-
-  uint8*p= top;
-
-  if(1&(intptr_t)p){
-    critbit0_node*q= (void*)(p-1);
-    traverse(q->child[0]);
-    traverse(q->child[1]);
-    free(q);
-  }else{
-    free(p);
-  }
-
-  /*:22*/
-
-}
-
-void critbit0_clear(critbit0_tree*t)
+int bloom_init(struct bloom * bloom, int entries, double error)
 {
-  if(t->root)traverse(t->root);
-  t->root= NULL;
-}
+  bloom->ready = 0;
 
-/*:21*//*23:*/
-
-static int
-allprefixed_traverse(uint8*top,
-    int(*handle)(const char*,void*),void*arg){
-  /*26:*/
-
-  if(1&(intptr_t)top){
-    critbit0_node*q= (void*)(top-1);
-    int direction;
-    for(direction= 0;direction<2;++direction)
-      switch(allprefixed_traverse(q->child[direction],handle,arg)){
-        case 1:break;
-        case 0:return 0;
-        default:return-1;
-      }
+  if (entries < 1 || error == 0) {
     return 1;
   }
 
-  /*27:*/
+  bloom->entries = entries;
+  bloom->error = error;
 
-  return handle((const char*)top,arg);/*:27*/
+  double num = log(bloom->error);
+  double denom = 0.480453013918201; // ln(2)^2
+  bloom->bpe = -(num / denom);
 
-}
+  double dentries = (double)entries;
+  bloom->bits = (int)(dentries * bloom->bpe);
 
-int
-critbit0_allprefixed(critbit0_tree*t,const char*prefix,
-    int(*handle)(const char*,void*),void*arg){
-  const uint8*ubytes= (void*)prefix;
-  const size_t ulen= strlen(prefix);
-  uint8*p= t->root;
-  uint8*top= p;
-
-  if(!p)return 1;
-  /*24:*/
-
-  while(1&(intptr_t)p){
-    critbit0_node*q= (void*)(p-1);
-    uint8 c= 0;
-    if(q->byte<ulen)c= ubytes[q->byte];
-    const int direction= (1+(q->otherbits|c))>>8;
-    p= q->child[direction];
-    if(q->byte<ulen)top= p;
+  if (bloom->bits % 8) {
+    bloom->bytes = (bloom->bits / 8) + 1;
+  } else {
+    bloom->bytes = bloom->bits / 8;
   }
 
-  /*:24*/
+  bloom->hashes = (int)ceil(0.693147180559945 * bloom->bpe);  // ln(2)
 
-  /*25:*/
-
-  size_t i;
-  for(i= 0;i<ulen;++i){
-    if(p[i]!=ubytes[i])return 1;
+  bloom->bf = (unsigned char *)calloc(bloom->bytes, sizeof(unsigned char));
+  if (bloom->bf == NULL) {
+    return 1;
   }
 
-  /*:25*/
-
-
-  return allprefixed_traverse(top,handle,arg);
+  bloom->ready = 1;
+  return 0;
 }
 
-/*:23*/
+
+int bloom_check(struct bloom * bloom, const void * buffer, int len)
+{
+  return bloom_check_add(bloom, buffer, len, 0);
+}
 
 
+int bloom_add(struct bloom * bloom, const void * buffer, int len)
+{
+  return bloom_check_add(bloom, buffer, len, 1);
+}
 
 
-static void*
-critbit_copy(void* top){
-  uint8*p = top;
-  if(1&(intptr_t)p){
-    critbit0_node*original= (void*)(p-1);
-    critbit0_node*newnode = walloc(sizeof(critbit0_node));
+void bloom_print(struct bloom * bloom)
+{
+  (void)printf("bloom at %p\n", (void *)bloom);
+  (void)printf(" ->entries = %d\n", bloom->entries);
+  (void)printf(" ->error = %f\n", bloom->error);
+  (void)printf(" ->bits = %d\n", bloom->bits);
+  (void)printf(" ->bits per elem = %f\n", bloom->bpe);
+  (void)printf(" ->bytes = %d\n", bloom->bytes);
+  (void)printf(" ->hash functions = %d\n", bloom->hashes);
+}
 
-    newnode->child[0] = critbit_copy(original->child[0]);
-    newnode->child[1] = critbit_copy(original->child[1]);
-    newnode->byte = original->byte;
-    newnode->otherbits = original->otherbits;
-    return (void*)(1+(char*)newnode);
-  }else{
-    const char*u=top;
-    const size_t ulen= strlen(u);
-    char*x = walloc(ulen+1);
-    memcpy(x,u,ulen+1);
-    return x;
+
+void bloom_free(struct bloom * bloom)
+{
+  if (bloom->ready) {
+    free(bloom->bf);
   }
+  bloom->ready = 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -430,46 +292,30 @@ int main(int argc,char *argv[]) {
   int c;
   int i;
 
-  critbit0_tree tree = {0};
+  struct bloom bloom;
+  bloom_init(&bloom, 210687, 0.000001);
   // printf("\npointer %zu", tree.root);
 
-#ifdef compact
-#else
-  void **treeRoot = walloc(sizeof(void *));
-#endif
-  //printf("\nw pointer %zu", treeRoot);
 
   if (argc==3) {
-#ifdef COMPACT
-    useWalloc = false;
-#endif
     // printf("\npointer %zu", tree.root);
     FILE *dict;
     dict = fopen(argv[1],"r");
     while (fscanf(dict, "%s", word) == 1) {
-      if (!isupper(word[0])) { critbit0_insert(&tree, word); }
+      if (!isupper(word[0])) { bloom_add(&bloom, word, strlen(word)); }
     }
     // printf("\npointer %zu", tree.root);
 
 
-#ifdef COMPACT
-    useWalloc = true;
-
-    critbit0_tree newTree = {0};
-    newTree.root = critbit_copy(tree.root);
-
-
-    *treeRoot = newTree.root;
-#else
-    *treeRoot = tree.root;
-#endif
+    //*treeRoot = tree.root;
     //printf("offset: %d , newRoot: %p", currentOffset, *treeRoot);
 
-    return 0;
+   // return 0;
   } else {
-    tree.root = *treeRoot;
+    //tree.root = *treeRoot;
      //printf("\npointer %zu", tree.root);
   }
+
 
 
   i = 0;
@@ -487,17 +333,17 @@ int main(int argc,char *argv[]) {
     if (c == '\n' || c == ' ') {
       word[i] = '\0';
       lword[i] = '\0';
-      i = 0;
       if (word[0] == '\0') {
         printf("%c",c);
       } else if (cant) {
         printf("<%s>%c",word,c);
         cant = false;
-      } else if (critbit0_contains(&tree, lword)) {
+      } else if (bloom_check(&bloom, lword, i)) {
         printf("%s%c",word,c);
       } else {
         printf("<%s>%c",word,c);
       }
+      i = 0;
     } else {
       word[i] = c;
       lword[i] = tolower(c);
